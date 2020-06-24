8361ead9026012dea2b12e4657a0a6062b7f86fa
Daniel.Kostelecky <Daniel.Kostelecky@CZPRL105P>
2020-06-24 23:37:58 +0200

    finishing layout

--- TFS_VPN_Logger/Form1.Designer.cs
+++ TFS_VPN_Logger/Form1.Designer.cs
@@ -47,6 +47,13 @@
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iconPictureBox3 = new FontAwesome.Sharp.IconPictureBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.iconPictureBox4 = new FontAwesome.Sharp.IconPictureBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.panelmenu.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
@@ -54,13 +61,19 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconuser)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // panelmenu
             // 
             this.panelmenu.BackColor = System.Drawing.Color.LightGray;
+            this.panelmenu.Controls.Add(this.button3);
+            this.panelmenu.Controls.Add(this.label3);
             this.panelmenu.Controls.Add(this.panel2);
+            this.panelmenu.Controls.Add(this.iconPictureBox4);
             this.panelmenu.Controls.Add(this.panel1);
+            this.panelmenu.Controls.Add(this.textBox1);
             this.panelmenu.Dock = System.Windows.Forms.DockStyle.Right;
             this.panelmenu.Location = new System.Drawing.Point(-415, 0);
             this.panelmenu.Margin = new System.Windows.Forms.Padding(2);
@@ -72,6 +85,9 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.iconPictureBox3);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.listBox2);
@@ -86,7 +102,7 @@
             this.panel2.Location = new System.Drawing.Point(415, 120);
             this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(586, 755);
+            this.panel2.Size = new System.Drawing.Size(641, 613);
             this.panel2.TabIndex = 2;
             // 
             // buttonAmbica
@@ -95,7 +111,7 @@
             this.buttonAmbica.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonAmbica.Font = new System.Drawing.Font("Exo", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.buttonAmbica.ForeColor = System.Drawing.Color.Black;
-            this.buttonAmbica.Location = new System.Drawing.Point(461, 14);
+            this.buttonAmbica.Location = new System.Drawing.Point(486, 48);
             this.buttonAmbica.Name = "buttonAmbica";
             this.buttonAmbica.Size = new System.Drawing.Size(117, 43);
             this.buttonAmbica.TabIndex = 9;
@@ -108,7 +124,7 @@
             this.button602.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button602.Font = new System.Drawing.Font("Exo", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button602.ForeColor = System.Drawing.Color.Black;
-            this.button602.Location = new System.Drawing.Point(327, 14);
+            this.button602.Location = new System.Drawing.Point(338, 48);
             this.button602.Name = "button602";
             this.button602.Size = new System.Drawing.Size(117, 43);
             this.button602.TabIndex = 8;
@@ -121,7 +137,7 @@
             this.buttonCIS.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonCIS.Font = new System.Drawing.Font("Exo", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.buttonCIS.ForeColor = System.Drawing.Color.Black;
-            this.buttonCIS.Location = new System.Drawing.Point(192, 14);
+            this.buttonCIS.Location = new System.Drawing.Point(192, 48);
             this.buttonCIS.Name = "buttonCIS";
             this.buttonCIS.Size = new System.Drawing.Size(117, 43);
             this.buttonCIS.TabIndex = 7;
@@ -134,7 +150,7 @@
             this.labelvendor.BackColor = System.Drawing.Color.Transparent;
             this.labelvendor.Font = new System.Drawing.Font("Exo", 20F, System.Drawing.FontStyle.Bold);
             this.labelvendor.ForeColor = System.Drawing.Color.White;
-            this.labelvendor.Location = new System.Drawing.Point(65, 19);
+            this.labelvendor.Location = new System.Drawing.Point(65, 53);
             this.labelvendor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelvendor.Name = "labelvendor";
             this.labelvendor.Size = new System.Drawing.Size(107, 38);
@@ -147,7 +163,7 @@
             this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Users;
             this.iconPictureBox1.IconColor = System.Drawing.Color.White;
             this.iconPictureBox1.IconSize = 44;
-            this.iconPictureBox1.Location = new System.Drawing.Point(26, 21);
+            this.iconPictureBox1.Location = new System.Drawing.Point(26, 55);
             this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.iconPictureBox1.Name = "iconPictureBox1";
             this.iconPictureBox1.Size = new System.Drawing.Size(46, 44);
@@ -170,9 +186,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::TFS_VPN_Logger.Properties.Resources.unnamed;
-            this.pictureBox1.Location = new System.Drawing.Point(416, -18);
+            this.pictureBox1.Location = new System.Drawing.Point(412, -31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(120, 156);
+            this.pictureBox1.Size = new System.Drawing.Size(126, 183);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -219,12 +235,12 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Exo", 20F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(65, 107);
+            this.label1.Location = new System.Drawing.Point(65, 160);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(288, 38);
+            this.label1.Size = new System.Drawing.Size(294, 38);
             this.label1.TabIndex = 11;
-            this.label1.Text = "Users to be  unlocked";
+            this.label1.Text = "Users to be  unlocked:";
             // 
             // iconPictureBox2
             // 
@@ -232,7 +248,7 @@
             this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.ListUl;
             this.iconPictureBox2.IconColor = System.Drawing.Color.White;
             this.iconPictureBox2.IconSize = 44;
-            this.iconPictureBox2.Location = new System.Drawing.Point(26, 109);
+            this.iconPictureBox2.Location = new System.Drawing.Point(26, 162);
             this.iconPictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.iconPictureBox2.Name = "iconPictureBox2";
             this.iconPictureBox2.Size = new System.Drawing.Size(46, 44);
@@ -253,14 +269,15 @@
             "Item3",
             "Item4",
             "Item5"});
-            this.listBox1.Location = new System.Drawing.Point(26, 176);
+            this.listBox1.Location = new System.Drawing.Point(72, 223);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(146, 112);
+            this.listBox1.Size = new System.Drawing.Size(211, 140);
             this.listBox1.TabIndex = 12;
             // 
             // listBox2
             // 
-            this.listBox2.BackColor = System.Drawing.Color.PaleGreen;
+            this.listBox2.BackColor = System.Drawing.Color.White;
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listBox2.Font = new System.Drawing.Font("Exo", 15F);
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 28;
@@ -270,9 +287,9 @@
             "Item3",
             "Item4",
             "Item5"});
-            this.listBox2.Location = new System.Drawing.Point(258, 176);
+            this.listBox2.Location = new System.Drawing.Point(379, 223);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(146, 116);
+            this.listBox2.Size = new System.Drawing.Size(199, 140);
             this.listBox2.TabIndex = 13;
             // 
             // button2
@@ -281,7 +298,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Exo", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(192, 248);
+            this.button2.Location = new System.Drawing.Point(311, 307);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(33, 35);
             this.button2.TabIndex = 15;
@@ -294,12 +311,98 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Exo", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(192, 189);
+            this.button1.Location = new System.Drawing.Point(311, 248);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(33, 35);
             this.button1.TabIndex = 16;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Exo", 20F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(65, 427);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 38);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Allowed time-frame to:";
+            // 
+            // iconPictureBox3
+            // 
+            this.iconPictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.Clock;
+            this.iconPictureBox3.IconColor = System.Drawing.Color.White;
+            this.iconPictureBox3.IconSize = 44;
+            this.iconPictureBox3.Location = new System.Drawing.Point(26, 429);
+            this.iconPictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.iconPictureBox3.Name = "iconPictureBox3";
+            this.iconPictureBox3.Size = new System.Drawing.Size(46, 44);
+            this.iconPictureBox3.TabIndex = 17;
+            this.iconPictureBox3.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.Maroon;
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.Gold;
+            this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.Color.Blue;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Exo", 15F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(71, 507);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(384, 34);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1131, 233);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(463, 292);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Exo", 20F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(1124, 173);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(390, 38);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Mandatory description (why):";
+            // 
+            // iconPictureBox4
+            // 
+            this.iconPictureBox4.BackColor = System.Drawing.Color.LightGray;
+            this.iconPictureBox4.ForeColor = System.Drawing.Color.DimGray;
+            this.iconPictureBox4.IconChar = FontAwesome.Sharp.IconChar.Clock;
+            this.iconPictureBox4.IconColor = System.Drawing.Color.DimGray;
+            this.iconPictureBox4.IconSize = 44;
+            this.iconPictureBox4.Location = new System.Drawing.Point(1085, 175);
+            this.iconPictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.iconPictureBox4.Name = "iconPictureBox4";
+            this.iconPictureBox4.Size = new System.Drawing.Size(46, 44);
+            this.iconPictureBox4.TabIndex = 20;
+            this.iconPictureBox4.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(24)))), ((int)(((byte)(39)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Exo", 18.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(1185, 620);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(358, 52);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "PROCEED";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // FormMain
             // 
@@ -317,6 +420,7 @@
             this.Text = "VPN Administrator by Daniel Kostelecky - ver 1.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panelmenu.ResumeLayout(false);
+            this.panelmenu.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
@@ -325,6 +429,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconuser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -349,6 +455,13 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
